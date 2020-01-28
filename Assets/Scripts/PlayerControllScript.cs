f635b5e64e8b045434783afa99829bb9d720c2fe
Eriasu17 <eliasaussenegg@gmail.com>
2020-01-28 14:55:44 +0100

    modifing Scripts

---
+++ Assets/Scripts/PlayerControllScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllScript : MonoBehaviour
+{
+    public float speed = 18;
+
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(hAxis, vAxis, 0) * speed * Time.deltaTime;
+        rig.MovePosition(transform.position + movement);
+    }
+}
---
+++ Assets/Scripts/WormController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (x > 10)
+        {
+            Debug.Log("x ist größer als 10");
+        }
+        else
+        {
+            Debug.Log("x ist kleiner als 10");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
