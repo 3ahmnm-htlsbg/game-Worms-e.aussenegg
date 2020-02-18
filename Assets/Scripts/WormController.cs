f14a21afa0603a0c75996cb0dc38a9e00d6ee749
Eriasu17 <eliasaussenegg@gmail.com>
2020-02-18 15:06:31 +0100

    finished Movment

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-   public void Update()
-   {
-        if (Input.GetKeyDown(KeyCode.W))
+
+    private Rigidbody rbA;
+    private Rigidbody rbB;
+    public GameObject PlayerA;
+    public GameObject PlayerB;
+
+    public void Start()
+    {
+      rbA = PlayerA.GetComponent<Rigidbody>();
+      rbB = PlayerB.GetComponent<Rigidbody>();
+      PlayerA = GameObject.FindGameObjectWithTag("PlayerA");
+      PlayerB = GameObject.FindGameObjectWithTag("PlayerB");
+    }
+
+    public void Update()
+    {
+        if(PlayerA == true)
         {
-            Debug.Log("Player moves Up!");
+            if (Input.GetKey(KeyCode.A))
+            {
+                rbA.AddForce(-2.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rbA.AddForce(2.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            
         }
-   }
+
+        if(PlayerB == true)
+        {
+            if (Input.GetKey(KeyCode.J))
+            {
+                rbB.AddForce(-2.5f, 0f, 0f, ForceMode.Impulse);
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                rbB.AddForce(2.5f, 0f, 0f, ForceMode.Impulse);
+            }
+        }
+       
+    }
 }
